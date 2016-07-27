6854e9b58639f53ea5a88f2914aa97c2676f8ee8
Tara Strauss <tara.m.strauss@gmail.com>
2016-07-27 15:31:05 -0700

    creates custome User class that extends abstractuser

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+
+class User(AbstractUser):
+  active_chef = models.BooleanField(default=False,
+                                    help_text='True if the user is a chef.')
+  profile_pic = models.URLField(blank=True, null=True,
+                                help_text='URL pointing to the users profile picture')
+
+  objects = UserManager()
