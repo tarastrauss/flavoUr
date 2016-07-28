25ff5595c2dff184f17253b9cf12d191f417d4b9
alee <alee046@ucr.edu>
2016-07-28 00:22:05 -0700

    landing page fixes

--- flavoUr/views.py
+++ flavoUr/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import get_object_or_404, render_to_response
 
-from django.template import loader
-from django.template import RequestContext
+from django.template import loader, RequestContext
 
 from django.http import HttpResponseRedirect, HttpResponse, Http404
-
 
 
 def home(request):
