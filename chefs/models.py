3623a27346d209e231580697371e7c327bc02bda
alee <alee046@ucr.edu>
2016-08-04 17:39:33 -0700

    chef model created

---
+++ chefs/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+from users.models import User
+from django.db import models
+
+class ChefInfo(models.Model):
+    '''
+    Represents the Chef Info profile for a User.
+    '''
+
+    user_info = models.OneToOneField(User, help_text="Connects user"
+                                    "to the ChefInfo class.",
+                                    primary_key=True)
+    star_rating = models.IntegerField(default=0, help_text='average value of'
+                                     'star ratings')
+
