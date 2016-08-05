74b8ce4f83903f5dd764f640d09e12a9ca841b3e
alee <alee046@ucr.edu>
2016-08-04 18:41:01 -0700

    patron/chef tests, unicode method to return firstname

--- chefs/tests.py
+++ chefs/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from users.models import User
+from chefs.models import ChefInfo
 
-# Create your tests here.
+class ChefTestCase(TestCase):
+
+    def setUp(self):
+
+        self.test_user = User.objects.create(username="Anthony")
+
+    def test_chef_creation(self):
+        '''
+        Makes sure ChefInfo model is created and connected to user model.
+        '''
+
+        # Creates ChefInfo object
+        chef_info = ChefInfo.objects.create(user_info=self.test_user)
+
+        # Asserts chef object is created and linked to user
+        self.assertEqual(1, len(ChefInfo.objects.all()))
+        self.assertEqual(self.test_user.id, chef_info.user_info.id)
+        self.assertEqual(chef_info.pk, self.test_user.chefinfo.pk)
+
--- patrons/models.py
+++ patrons/models.py
@@ -4,11 +4,14 @@ from django.db import models
 from annoying.fields import AutoOneToOneField
 
 class PatronInfo(models.Model):
-  '''
-  Represents the Patron Info profile for a User.
-  '''
+    '''
+    Represents the Patron Info profile for a User.
+    '''
 
-  user_info = AutoOneToOneField(User, help_text='Automatically connects user'
+    user_info = AutoOneToOneField(User, help_text='Automatically connects user'
                                   'to the PatronInfo class when a useris created',
                                   primary_key=True)
 
+    def __unicode__(self):
+
+        return self.user_info.first_name
