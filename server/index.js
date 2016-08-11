d0c7a0fd8d9e45f0b5122242950c7f4543c6f34c
alee <alee046@ucr.edu>
2016-08-10 20:44:20 -0700

    redux added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './generated/app';
 
 const app = express();
@@ -16,9 +18,20 @@ app.use(express.static(path.resolve(__dirname, '../dist')));
 
 // Routes
 app.get('/', (request, response) => {
+  const initialState = {
+    currentReview: '',
+    reviews: []
+  };
+  const store = createStore((state=initialState) => state);
+  const appContent = ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
   response.render('app', {
-    app: ReactDOMServer.renderToString(<App />)
+    app: appContent,
+    initialState: JSON.stringify(initialState)
   });
 });
-
 app.listen(3000, () => console.log('Server running'));
