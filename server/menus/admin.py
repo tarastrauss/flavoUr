2bf7cd92ed138ac88541a275a36736fe690928fc
Tara Strauss <tara.m.strauss@gmail.com>
2016-08-18 16:40:52 -0700

    gets menu and item views to work with all CRUD functionality

--- server/menus/admin.py
+++ server/menus/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from menus.models import Menu, Item
 # Register your models here.
+
+class ItemInline(admin.TabularInline):
+    model = Item
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [ItemInline]
+
+admin.site.register(Menu, ItemAdmin)
