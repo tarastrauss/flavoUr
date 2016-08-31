c0f25ae86e3ccaba17a1225a1dc217d0d88c121a
alee <alee046@ucr.edu>
2016-08-30 17:18:29 -0700

    POST action unit tests for item-list and menu-list

--- server/menus/tests.py
+++ server/menus/tests.py
@@ -4,7 +4,7 @@ from users.models import User
 from chefs.models import Chef
 from menus.models import Menu, Item
 from django.core.urlresolvers import reverse
-
+from rest_framework import status
 class MenuTestCase(TestCase):
 
     def setUp(self):
@@ -33,6 +33,9 @@ class MenuTestCase(TestCase):
         # self.assertEqual(menu, item.menu)
 
     def test_menu_get(self):
+        '''
+        Tests get endpoints for menu-detail and item-detail
+        '''
         menu = Menu.objects.create(chef=self.chef, title='foxy food', delivery=True )
         item = Item.objects.create(menu=menu, title="kibbles", size="the whole bowl",
                                    price=13.33, description="food for foxy")
@@ -40,6 +43,30 @@ class MenuTestCase(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.data, {'id': 1, 'chef':1, 'title':'foxy food', 'delivery':True})
 
-        url = reverse('item-detail', kwargs={'pk':item.id})
+        url = reverse('item-detail', kwargs={'menu_pk':menu.id,'pk':item.id})
         response = self.client.get(url)
         self.assertEqual(response.data, {'id':1, 'title':'kibbles', 'size':'the whole bowl', 'price':'13.33', 'description':'food for foxy', 'menu':1})
+
+    def test_menu_post(self):
+        '''
+        Tests POST method for menu-list.
+        '''
+        url = reverse('menu-list')
+        data = { 'chef':1, 'title':'just the best', 'delivery':True}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Menu.objects.count(), 1)
+        self.assertEqual(Menu.objects.get().title, 'just the best')
+
+    def test_item_post(self):
+        '''
+        Tests POST method for item-list.
+        '''
+        menu = Menu.objects.create(chef=self.chef, title='foxy food', delivery=True )
+        url = reverse('item-list', kwargs={'pk':menu.id})
+        data = {'title':'foxy snacks', 'size':'all of it', 'price':'10.00', 'description':'food for foxy', 'menu':1}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.get().title, 'foxy snacks')
+
