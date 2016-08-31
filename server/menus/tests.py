3ec4cef625923d35ea32bc5c01465c851f0603de
alee <alee046@ucr.edu>
2016-08-30 18:57:14 -0700

    test fixes

--- server/menus/tests.py
+++ server/menus/tests.py
@@ -5,6 +5,7 @@ from chefs.models import Chef
 from menus.models import Menu, Item
 from django.core.urlresolvers import reverse
 from rest_framework import status
+
 class MenuTestCase(TestCase):
 
     def setUp(self):
@@ -12,6 +13,7 @@ class MenuTestCase(TestCase):
         self.test_user = User.objects.create(username="Foxy",
                                              first_name="Foxy")
         self.chef = Chef.objects.create(user=self.test_user)
+        self.menu = Menu.objects.create(chef=self.chef, title='foxy food', delivery=True )
 
     def test_menu_creation(self):
         '''
@@ -34,16 +36,15 @@ class MenuTestCase(TestCase):
 
     def test_menu_get(self):
         '''
-        Tests get endpoints for menu-detail and item-detail
+        Tests GET metho for menu-detail and item-detail
         '''
-        menu = Menu.objects.create(chef=self.chef, title='foxy food', delivery=True )
-        item = Item.objects.create(menu=menu, title="kibbles", size="the whole bowl",
+        item = Item.objects.create(menu=self.menu, title="kibbles", size="the whole bowl",
                                    price=13.33, description="food for foxy")
-        url = reverse('menu-detail', kwargs={'pk':menu.id})
+        url = reverse('menu-detail', kwargs={'pk':self.menu.id})
         response = self.client.get(url)
         self.assertEqual(response.data, {'id': 1, 'chef':1, 'title':'foxy food', 'delivery':True})
 
-        url = reverse('item-detail', kwargs={'menu_pk':menu.id,'pk':item.id})
+        url = reverse('item-detail', kwargs={'menu_pk':self.menu.id,'pk':item.id})
         response = self.client.get(url)
         self.assertEqual(response.data, {'id':1, 'title':'kibbles', 'size':'the whole bowl', 'price':'13.33', 'description':'food for foxy', 'menu':1})
 
@@ -55,18 +56,15 @@ class MenuTestCase(TestCase):
         data = { 'chef':1, 'title':'just the best', 'delivery':True}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Menu.objects.count(), 1)
-        self.assertEqual(Menu.objects.get().title, 'just the best')
+        self.assertTrue(Menu.objects.filter(title='just the best').exists())
 
     def test_item_post(self):
         '''
         Tests POST method for item-list.
         '''
-        menu = Menu.objects.create(chef=self.chef, title='foxy food', delivery=True )
-        url = reverse('item-list', kwargs={'pk':menu.id})
+        url = reverse('item-list', kwargs={'pk':self.menu.id})
         data = {'title':'foxy snacks', 'size':'all of it', 'price':'10.00', 'description':'food for foxy', 'menu':1}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Item.objects.count(), 1)
-        self.assertEqual(Item.objects.get().title, 'foxy snacks')
+        self.assertTrue(Item.objects.filter(title= 'foxy snacks').exists())
 
